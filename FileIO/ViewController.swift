5e2a6ad6d038bee569a9072ee816be8b9cd93928
chung <chungvtgc00989@fpt.edu.vn>
2016-09-01 10:25:33 +0700

    first update

--- FileIO/ViewController.swift
+++ FileIO/ViewController.swift
@@ -9,22 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var citys: [String] = ["ha noi", "hai phong"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        readFileFromAppDirectory("data.txt")
-        readWriteFile()
+        //        readFileFromAppDirectory("data.txt")
+        //        readWriteFile()
+        let fm: FileMan = FileMan()
+//        fm.readLines("/Users/chung/Documents/test.txt");
+//        fm.appendStringToFile("/Users/chung/Documents/test3.txt", text: "Add theem")
+//        fm.appendArrayToFile("/Users/chung/Documents/test2.txt", array: citys)
+        fm.readWords("/Users/chung/Documents/SHA1.txt")
+        
         
     }
     func readFileFromAppDirectory(file: String) {
         
+        
         if let path = NSBundle.mainBundle().URLForResource("data", withExtension: "txt"){
-            //reading
+            //            reading
             do {
                 let text = try NSString(contentsOfURL: path, encoding: NSUTF8StringEncoding)
                 print (text)
             }
             catch {/* error handling here */}
+            
         }
     }
     func readWriteFile() {
@@ -49,6 +57,6 @@ class ViewController: UIViewController {
             catch {/* error handling here */}
         }
     }
-
+    
 }
 
